d2bc88fb2fcd879522f290a6e56c9a96ab2ff49c
gp1108 <guillermopellew@hotmail.es>
2023-09-13 16:54:05 +0200

    NavMeshPackage(PRO) implementation
    Download and implementation of and external asset for navmesh

---
+++ Assets/Scripts/NavMeshAI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+using System.IO;
+
+
+public class NavMeshAI : MonoBehaviour
+{
+    public GameObject targetDestination;
+    private NavMeshAgent _agent;
+
+    public NavMeshSurface _surface;
+   
+
+
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+       _surface = GetComponent<NavMeshSurface>();
+    }
+
+    
+    void Update()
+    {
+        //Hacer que el agente vaya al target deseado
+        _agent.destination = targetDestination.transform.position;
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    _surface.BuildNavMesh();
+        //}
+    }
+}
+
