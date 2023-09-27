6214c51cb59a0adc20b1505772f540fdf9dff259
gp1108 <guillermopellew@hotmail.es>
2023-09-27 14:33:23 +0200

    PreviewSystemBeta
    Queda arreglar estructuras de mayor tamaño , ahora mismo no estan funcionando , subo el codgio para facil acceso

--- Assets/Scripts/PreviewPrefabSize.cs
+++ Assets/Scripts/PreviewPrefabSize.cs
@@ -45,12 +45,62 @@ public class PreviewPrefabSize : MonoBehaviour
         }
         else if(prefabSize == 1)
         {
+            //PENDIENTE DE COMPROBACION MATEMATICA
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -transform.up, out hit, 0.4f) &&
+                Physics.Raycast(new Vector3( transform.position.x-1,transform.position.y, transform.position.z-1), -transform.up, out hit, 0.4f) &&
+                Physics.Raycast(new Vector3(transform.position.x + 1, transform.position.y, transform.position.z - 1), -transform.up, out hit, 0.4f) &&
+                Physics.Raycast(new Vector3(transform.position.x - 1, transform.position.y, transform.position.z + 1), -transform.up, out hit, 0.4f) &&
+                Physics.Raycast(new Vector3(transform.position.x + 1, transform.position.y, transform.position.z + 1), -transform.up, out hit, 0.4f))
+            {
+                if (hit.collider.CompareTag("Node"))
+                {
 
+                    if (hit.collider.gameObject.GetComponent<Nodes>().constructed == false)
+                    {
+                        Debug.Log("·esto es un nodo");
+                        validposition = true;
+                    }
+                    else
+                    {
+                        validposition = false;
+
+                    }
+
+                }
+
+
+            }
 
         }
         else if(prefabSize ==2)
         {
+            //nO FUNCIONA , DIBUJAR LOS RAYOS Y ANALIAR QUE PASA
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -transform.up, out hit, 0.4f) &&
+                Physics.Raycast(new Vector3(transform.position.x - 1.5f, transform.position.y, transform.position.z - 1.5f), -transform.up, out hit, 0.4f) &&
+                Physics.Raycast(new Vector3(transform.position.x + 1.5f, transform.position.y, transform.position.z - 1.5f), -transform.up, out hit, 0.4f) &&
+                Physics.Raycast(new Vector3(transform.position.x - 1.5f, transform.position.y, transform.position.z + 1.5f), -transform.up, out hit, 0.4f) &&
+                Physics.Raycast(new Vector3(transform.position.x + 1.5f, transform.position.y, transform.position.z + 1.5f), -transform.up, out hit, 0.4f))
+            {
+                if (hit.collider.CompareTag("Node"))
+                {
 
+                    if (hit.collider.gameObject.GetComponent<Nodes>().constructed == false)
+                    {
+                        Debug.Log("·esto es un nodo");
+                        validposition = true;
+                    }
+                    else
+                    {
+                        validposition = false;
+
+                    }
+
+                }
+
+
+            }
         }
     }
 }
