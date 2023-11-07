19bc84a337cac39bc5949876854fccfbb628504f
QunhaoLiu <qunhao12345@gmail.com>
2023-11-07 19:31:59 +0100

    Cambio de color
    Colores mas oscuros fuera y mas transparentes dentro

--- Assets/Scripts/RangeIndicador.cs
+++ Assets/Scripts/RangeIndicador.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RangeIndicador : MonoBehaviour
 {
     public GameObject rangeIndicator;
+    public GameObject rangeIndicator2;
     private bool mostrarRango = false;
 
     // Start is called before the first frame update
@@ -26,12 +27,14 @@ public class RangeIndicador : MonoBehaviour
     private void MostrarRango()
     {
         rangeIndicator.SetActive(true);
+        rangeIndicator2.SetActive(true);
         mostrarRango=true;
     }
 
     private void OcultarRango()
     {
         rangeIndicator.SetActive(false);
+        rangeIndicator2.SetActive(false);
         mostrarRango=false;
     }
 
