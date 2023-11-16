f0910bef115f31ebdb535328dd110902dcbae0a4
gp1108 <guillermopellew@hotmail.es>
2023-11-16 19:25:27 +0100

    NewSkillTree(Beta.2)
    Esqueleto del skill tree funcional, mitad del arbol implementado. Sigue quedando toda la logica del codigo

--- Assets/Scripts/Skills.cs
+++ Assets/Scripts/Skills.cs
@@ -52,13 +52,13 @@ public class Skills : MonoBehaviour
     {
         { SkillName.moreHealthTurrets, 1 },
         { SkillName.moreDamageTurrets, 1 },
-        { SkillName.moreHealthWalls, 1000 },
+        { SkillName.moreHealthWalls, 1 },
         { SkillName.unlockSniperTurret,2 },
         { SkillName.unlockMachinegunTurret,2},
         { SkillName.sniperTurretMoreFireRate, 1 },
         { SkillName.structureRecoverHealth,2},
         { SkillName.unlockLaserTurret,2 },
-        { SkillName.unlockMortarTurret,2},
+        { SkillName.unlockMortarTurret,200},
 
         { SkillName.unlockMines,2},
         { SkillName.oneMoreMine,2},
@@ -68,6 +68,8 @@ public class Skills : MonoBehaviour
         { SkillName.unlockGems,2},
         { SkillName.discountTurrets,2}
     };
+
+    
 
     //Define si ha sido desbloqueado o no 
     public Dictionary<SkillName, bool> isSkillUnlocked = new Dictionary<SkillName, bool>
@@ -91,11 +93,33 @@ public class Skills : MonoBehaviour
         { SkillName.discountTurrets,false}
     };
 
+    public Dictionary<SkillName, bool> skillCanBeUnlocked = new Dictionary<SkillName, bool>
+    {
+        { SkillName.moreHealthTurrets, true },
+        { SkillName.moreDamageTurrets, false },
+        { SkillName.moreHealthWalls, false },
+        { SkillName.unlockSniperTurret,false },
+        { SkillName.unlockMachinegunTurret,false},
+        { SkillName.sniperTurretMoreFireRate, false },
+        { SkillName.structureRecoverHealth,false},
+        { SkillName.unlockLaserTurret,false },
+        { SkillName.unlockMortarTurret,false},
+
+        { SkillName.unlockMines,false},
+        { SkillName.oneMoreMine,false},
+        { SkillName.fasterResearch,false},
+        { SkillName.fastMine,false},
+        { SkillName.slowMine,false},
+        { SkillName.unlockGems,false},
+        { SkillName.discountTurrets,false}
+    };
+
     public List<GameObject> SkillButtons = new List<GameObject>();
 
     public Color unlockedSkillColor;
     public Color notEnoughRPcolor;
     public Color defaultColor;
+    public Color dependency;
     private GameObject canvas;
     private void Start()
     {
@@ -118,14 +142,141 @@ public class Skills : MonoBehaviour
     {
         if (skillCost[skill] <= gameManager.giveMeReference.researchPoints)
         {
-            Debug.Log("He desbloqueado la habilidad" + skill.ToString());
-            gameManager.giveMeReference.researchPoints -= skillCost[skill];
-            isSkillUnlocked[skill] = true;
-            DoSkill(skill);
-            UpdateSkillUI();
-            
+
+            switch (skill)
+            {
+                case SkillName.moreHealthTurrets:
+
+                    skillCanBeUnlocked[SkillName.moreDamageTurrets] = true;
+                    skillCanBeUnlocked[SkillName.moreHealthWalls] = true;
+                    //logica
+                    UnlockSkillLogic(skill);
+
+                    break;
+                case SkillName.moreDamageTurrets:
+                    if (isSkillUnlocked[SkillName.moreHealthTurrets] == true)
+                    {
+
+                        skillCanBeUnlocked[SkillName.unlockSniperTurret] = true;
+                        skillCanBeUnlocked[SkillName.unlockMachinegunTurret] = true;
+
+                        UnlockSkillLogic(skill);
+                        
+                        //Logica
+                    }
+                    break;
+                case SkillName.moreHealthWalls:
+                    if (isSkillUnlocked[SkillName.moreHealthTurrets] == true)
+                    {
+                        skillCanBeUnlocked[SkillName.structureRecoverHealth] = true;
+
+
+                        UnlockSkillLogic(skill);
+
+                        //Logica
+                    }
+                    break;
+                case SkillName.unlockSniperTurret:
+                    if (isSkillUnlocked[SkillName.moreDamageTurrets] == true)
+                    {
+                        skillCanBeUnlocked[SkillName.sniperTurretMoreFireRate] = true;
+
+
+                        UnlockSkillLogic(skill);
+
+                        //Logica
+                    }
+                    break;
+                case SkillName.unlockMachinegunTurret:
+                    if (isSkillUnlocked[SkillName.moreDamageTurrets] == true)
+                    {
+                        skillCanBeUnlocked[SkillName.unlockLaserTurret] = true;
+                        skillCanBeUnlocked[SkillName.unlockMortarTurret] = true;
+
+                        UnlockSkillLogic(skill);
+
+                        //Logica
+                    }
+
+                    break;
+                case SkillName.structureRecoverHealth:
+                    if (isSkillUnlocked[SkillName.moreHealthWalls] == true)
+                    {
+                        //FALTA AÑADIR RECOVER HEALT 2 
+
+                        UnlockSkillLogic(skill);
+
+                        //Logica
+                    }
+                    break;
+
+                case SkillName.sniperTurretMoreFireRate:
+                    if (isSkillUnlocked[SkillName.unlockSniperTurret] == true)
+                    {
+                        
+
+                        UnlockSkillLogic(skill);
+
+                        //Logica
+                    }
+                    break;
+                case SkillName.unlockLaserTurret:
+                    if (isSkillUnlocked[SkillName.unlockMachinegunTurret] == true)
+                    {
+
+
+                        UnlockSkillLogic(skill);
+
+                        //Logica
+                    }
+                    break;
+                case SkillName.unlockMortarTurret:
+                    if (isSkillUnlocked[SkillName.unlockMachinegunTurret] == true)
+                    {
+
+
+                        UnlockSkillLogic(skill);
+
+                        //Logica
+                    }
+                    break;
+
+                case SkillName.unlockMines:
+
+                    break;
+                case SkillName.oneMoreMine:
+
+                    break;
+                case SkillName.fasterResearch:
+
+                    break;
+                case SkillName.fastMine:
+
+                    break;
+                case SkillName.slowMine:
+
+                    break;
+                case SkillName.unlockGems:
+
+                    break;
+                
+                case SkillName.discountTurrets:
+
+                    break;
+
+            }
+
+
         }
    
+    }
+
+    private void UnlockSkillLogic(SkillName skill)
+    {
+        Debug.Log("He desbloqueado la habilidad" + skill.ToString());
+        gameManager.giveMeReference.researchPoints -= skillCost[skill];
+        isSkillUnlocked[skill] = true;
+        UpdateSkillUI();
     }
     
     public void UpdateSkillUI()
@@ -135,7 +286,7 @@ public class Skills : MonoBehaviour
             SkillName clave = kvp.Key;
             int valor = kvp.Value;
             
-            if(kvp.Value > gameManager.giveMeReference.researchPoints && isSkillUnlocked[kvp.Key] == false)
+            if(kvp.Value > gameManager.giveMeReference.researchPoints && isSkillUnlocked[kvp.Key] == false && skillCanBeUnlocked[kvp.Key] == true)
             {
                 foreach(GameObject skillButtons in SkillButtons)
                 {
@@ -169,57 +320,22 @@ public class Skills : MonoBehaviour
                     }
                 }
             }
+            if (skillCanBeUnlocked[kvp.Key] == false )
+            {
+
+                foreach (GameObject skillButtons in SkillButtons)
+                {
+
+                    if (skillButtons.gameObject.name == kvp.Key.ToString())
+                    {
+
+                        skillButtons.gameObject.GetComponent<Image>().color = dependency;
+                        //skillButtons.gameObject.GetComponent<Button>().interactable = false;
+                    }
+                }
+            }
         }
     }
 
-    public void DoSkill(SkillName skill)
-    {
-        switch(skill)
-        {
-            case SkillName.moreHealthTurrets:
 
-                break;
-            case SkillName.moreDamageTurrets:
-
-                break;
-            case SkillName.moreHealthWalls:
-
-                break;
-            case SkillName.unlockSniperTurret:
-
-                break;
-            case SkillName.unlockMachinegunTurret:
-
-                break;
-
-            case SkillName.unlockMines:
-
-                break;
-            case SkillName.oneMoreMine:
-
-                break;
-            case SkillName.fasterResearch:
-
-                break;
-            case SkillName.fastMine:
-
-                break;
-            case SkillName.slowMine:
-
-                break;
-            case SkillName.unlockGems:
-
-                break;
-            case SkillName.structureRecoverHealth:
-
-                break;
-            case SkillName.discountTurrets:
-
-                break;
-              
-        }
-
-        
-
-    }
 }
