ce9db5d229d4deb609bae3feb5cfa184669f319b
QunhaoLiu <qunhao12345@gmail.com>
2024-01-10 20:47:50 +0100

    Pequeños cambios
    pequeños cambios en los sonidos

--- Assets/Scripts/SoundControl.cs
+++ Assets/Scripts/SoundControl.cs
@@ -1,56 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SoundControl : MonoBehaviour
 {
-    public static float _musicVolume {  get; private set; }
-    public static float _SFXVolume { get; private set; }
+    public static float _musicVolume { get; private set; } = 1f;
+    public static float _SFXVolume { get; private set; } = 1f;
+    public static float _TurretVolume { get; private set; } = 1f;
 
-    public static float _TurretVolume { get; private set; }
-
-    private const string prefMusicVolume = "prefMusicVolume";
-    private const string prefSFXVolume = "prefSFXVolume";
-    private const string prefTurretVolume = "prefTurretVolume";
 
     private void Start()
     {
-        if (PlayerPrefs.HasKey(prefMusicVolume))
-        {
-            _musicVolume = PlayerPrefs.GetFloat(prefMusicVolume);
-        }
+        SoundManager.instance.LoadMixerVolume();
 
-        if (PlayerPrefs.HasKey(prefSFXVolume))
-        {
-            _SFXVolume = PlayerPrefs.GetFloat(prefSFXVolume);
-        }
-
-        if (PlayerPrefs.HasKey(prefTurretVolume))
-        {
-            _TurretVolume = PlayerPrefs.GetFloat(prefTurretVolume);
-        }
     }
 
     public void OnMusicSliderValueChange(float value)
     {
         _musicVolume = value;
         SoundManager.instance.UpdateMixerVolume();
-        PlayerPrefs.SetFloat(prefMusicVolume, _musicVolume);
     }
 
     public void OnSFXSliderValueChange(float value)
     {
         _SFXVolume = value;
         SoundManager.instance.UpdateMixerVolume();
-        PlayerPrefs.SetFloat(prefSFXVolume, _SFXVolume);
     }
 
     public void OnTurretSliderValueChange(float value)
     {
         _TurretVolume = value;
         SoundManager.instance.UpdateMixerVolume();
-        PlayerPrefs.SetFloat(prefSFXVolume, _TurretVolume);
     }
+
+    public void GuardarValoresAudioMixer()
+    {
+        SoundManager.instance.SaveMixerVolume();
+    }
+
+    public void CargarValoresAudioMixer()
+    {
+        SoundManager.instance.LoadMixerVolume();
+    }
+
 
 }
--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -38,8 +38,11 @@ public class gameManager : MonoBehaviour
     private int _totalNumberOfEnemies;
     public bool onRound;
     public int enemiesAlive;
+    public int _enemiesDead;
     private int enemiesSpawned;
     public TMP_Text roundsText;
+    public TMP_Text _MaxRoundsCount;
+    //public TMP_Text _totalEnemiesDead;
     private GameObject enemyToSpawn;
     [Header("Menu Management")]
     public GameObject BuildMenuButton;
@@ -56,8 +59,10 @@ public class gameManager : MonoBehaviour
     [Header("Gold System")]
     public float gold;
     public TMP_Text goldText;
+    public TMP_Text spendGoldText;
     [Header("Research System")]
     public int researchPoints;
+    //public TMP_Text totalresearchPoint;
     [Header("NavMesh")]
     public GameObject navmeshUpdater;
     [Header("Number of ResearchLabs")]
@@ -90,12 +95,17 @@ public class gameManager : MonoBehaviour
         maxNumberOfLabs = 1;
         numberOfLabs = 0;
         onRound = false;
-       _roundsPlayed = 0;
-       _totalRounds = 20;
-       _totalNumberOfEnemies = 5;
+        _roundsPlayed = 0;
+        _totalRounds = 20;
+        _totalNumberOfEnemies = 5;
+        _enemiesDead = 0;
+        //_totalEnemiesDead.text = PlayerPrefs.GetInt("Enemies Dead",0).ToString();
         roundsText.text = "Ronda "  + _roundsPlayed.ToString();
+        _MaxRoundsCount.text = PlayerPrefs.GetInt("Round Played", 0).ToString();
         GetGold(1000 + PlayerPrefs.GetFloat("startWithMoreGold"));
-        GetResearchPoints(100 + Mathf.RoundToInt(PlayerPrefs.GetFloat("startWithMoreResearchPoints"))); 
+        spendGoldText.text = PlayerPrefs.GetFloat("Spend Gold", 0).ToString();
+        GetResearchPoints(100 + Mathf.RoundToInt(PlayerPrefs.GetFloat("startWithMoreResearchPoints")));
+        //totalresearchPoint.text = PlayerPrefs.GetInt("Total RP", 0).ToString();
 
     }
     private void Update()
@@ -171,7 +181,7 @@ public class gameManager : MonoBehaviour
         if(_roundsPlayed <= _totalRounds)
         {
             roundsText.text = "Ronda "  + _roundsPlayed.ToString();
-
+            PlayerPrefs.SetInt("Round Played", _roundsPlayed);
             StartCoroutine("Revision");
             
             
@@ -281,7 +291,7 @@ public class gameManager : MonoBehaviour
     public void EnemyDead()
     {
         enemiesAlive -= 1;
-
+        //PlayerPrefs.SetInt("Enemies dead", _enemiesDead);
         SoundManager.dameReferencia.PlayClipByName(clipName: "EnemyDead");
 
         if (enemiesAlive <= 0 && onRound == true && enemiesSpawned == _totalNumberOfEnemies) 
@@ -332,6 +342,7 @@ public class gameManager : MonoBehaviour
         lastGold = gold;
         gold += oro;
         goldText.text = gold.ToString();
+        PlayerPrefs.SetFloat("Spend Gold", gold);
         if(lastGold > gold )
         {
             Invoke("SonidoOro", 0.15f);
@@ -357,6 +368,7 @@ public class gameManager : MonoBehaviour
     {
 
         researchPoints += ResearchPoints;
+        //PlayerPrefs.GetInt("Total RP", researchPoints);
         //FALTA A�ADIR EL TEXTO DE LOS PUNTOS DE INVESTIGACION
 
     }
