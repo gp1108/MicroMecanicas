cfecfb68fca736363aabf206674395d0a002a6e6
QunhaoLiu <qunhao12345@gmail.com>
2024-01-16 19:10:46 +0100

    Nueva barra de vida y cambio de color al destruir
    se ha agregado un fondo a la vida de los enemigos, las estructuras se vuelven rojas cuando pones el ratón encima al destruir

--- Assets/Scripts/DestroyStructure.cs
+++ Assets/Scripts/DestroyStructure.cs
@@ -6,10 +6,92 @@ public class DestroyStructure : MonoBehaviour
 {
     private GameObject canvas;
 
+    [SerializeField] private Material changeColor;
+    private Color originalColor;
+
+    private void Start()
+    {
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+        Renderer[] childRenderers = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer childRenderer in childRenderers)
+        {
+            originalColor = childRenderers[0].material.color;
+            childRenderer.material.color = originalColor;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        bool isDestructiveModeActive = canvas.GetComponent<BuildMenuButton>().destroyModeActive;
+        if (isDestructiveModeActive == true)
+        {
+            Renderer[] childRenderers = GetComponentsInChildren<Renderer>();
+            foreach (Renderer childRenderer in childRenderers)
+            {
+                if (gameObject.tag == "Wall")
+                {
+                    Debug.Log("Enter");
+                    childRenderer.enabled = true;
+                    childRenderer.material = changeColor;
+                }
+                else
+                {
+                    if (gameObject.tag == "BaseTurret")
+                    {
+                        Debug.Log("Enter");
+                        childRenderer.material = changeColor;
+                    }
+                    else if (this.gameObject.tag == "OtherTurret")
+                    {
+                        Debug.Log("Enter");
+                        childRenderer.material = changeColor;
+                    }
+                    else if (this.gameObject.tag == "Taller")
+                    {
+                        Debug.Log("Enter");
+                        childRenderer.material = changeColor;
+                    }
+                    else if (this.gameObject.tag == "SniperTurret")
+                    {
+                        Debug.Log("Enter");
+                        childRenderer.material = changeColor;
+                    }
+                    else if (this.gameObject.tag == "LaserTurret")
+                    {
+                        Debug.Log("Enter");
+                        childRenderer.material = changeColor;
+                    }
+                    else if (this.gameObject.tag == "Mine")
+                    {
+                        Debug.Log("Enter");
+                        childRenderer.material = changeColor;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        volverColorOri();
+    }
+
+    public void volverColorOri()
+    {
+        Renderer[] childRenderers = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer childRenderer in childRenderers)
+        {
+            originalColor = childRenderers[0].material.color;
+            childRenderer.material.color = originalColor;
+        }
+    }
+
     private void OnMouseUpAsButton()
     {
         
-        canvas = GameObject.FindGameObjectWithTag("Canvas");
+        
         if (canvas.GetComponent<BuildMenuButton>().destroyModeActive == true)
         {
             
